6c93963e2615f57699e3e584025dca4e2f1f0d39
Alanis Gomez <agomez.aper@gmail.com>
2020-08-10 21:16:34 -0300

    Diseño product policy desktop

--- packages/Webkul/Shop/src/Resources/views/emails/layouts/logo.blade.php
+++ packages/Webkul/Shop/src/Resources/views/emails/layouts/logo.blade.php
@@ -1,5 +1,5 @@
 @if ($logo = core()->getCurrentChannel()->logo_url)
-    <img src="{{ $logo }}" alt="{{ config('app.name') }}" style="height: 40px; width: 110px;"/>
+    <img src="{{ $logo }}" alt="{{ config('app.name') }}"/>
 @else
     <img src="{{ asset('/themes/reinabatata/assets/images/Logo.svg') }}">
 @endif
