7995c07eda338efd647ab4c17403efb1927a7889
Alanis Gomez <agomez.aper@gmail.com>
2020-08-14 23:49:51 -0300

    Configuraciónes backend

--- packages/Webkul/Admin/src/Config/acl.php
+++ packages/Webkul/Admin/src/Config/acl.php
@@ -436,25 +436,6 @@ return [
         'name'  => 'admin::app.acl.delete',
         'route' => 'admin.catalog-rules.delete',
         'sort'  => 3,
-    ],
-    [
-        'key'        => 'reinabatata',
-        'name'       => 'reinabatata::app.admin.layouts.reinabatata',
-        'route'      => 'reinabatata.admin.content.index',
-        'sort'       => 5,
-        'icon-class' => 'reinabatata-icon',
-    ], [
-        'key'        => 'reinabatata.meta-data',
-        'name'       => 'reinabatata::app.admin.layouts.meta-data',
-        'route'      => 'reinabatata.admin.meta-data',
-        'sort'       => 1,
-        'icon-class' => '',
-    ], [
-        'key'        => 'reinabatata.header',
-        'name'       => 'reinabatata::app.admin.layouts.header-content',
-        'route'      => 'reinabatata.admin.content.index',
-        'sort'       => 2,
-        'icon-class' => '',
     ], [
         'key'        => 'cms',
         'name'       => 'admin::app.layouts.cms',
---
+++ packages/Webphorium/ReinaBatata/src/Config/acl.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        'key'        => 'reinabatata',
+        'name'       => 'reinabatata::app.admin.layouts.reinabatata',
+        'route'      => 'reinabatata.admin.content.index',
+        'sort'       => 5,
+        'icon-class' => 'reinabatata-icon',
+    ], [
+        'key'        => 'reinabatata.meta-data',
+        'name'       => 'reinabatata::app.admin.layouts.meta-data',
+        'route'      => 'reinabatata.admin.meta-data',
+        'sort'       => 1,
+        'icon-class' => '',
+    ], [
+        'key'        => 'reinabatata.header',
+        'name'       => 'reinabatata::app.admin.layouts.header-content',
+        'route'      => 'reinabatata.admin.content.index',
+        'sort'       => 2,
+        'icon-class' => '',
+    ]
+];
+?>
