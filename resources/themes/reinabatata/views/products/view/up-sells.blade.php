8996894905d2001f9b74fa2d5f8ed5075fc40d2e
Alanis Gomez <agomez.aper@gmail.com>
2020-08-20 09:21:04 -0300

    Responsive carousel fix

--- resources/themes/reinabatata/views/products/view/up-sells.blade.php
+++ resources/themes/reinabatata/views/products/view/up-sells.blade.php
@@ -32,7 +32,7 @@
     <div class="carousel-products vc-small-screen">
         <carousel-component
             :slides-count="{{ sizeof($productUpSells) }}"
-            :perPageCustom="[[320, 1], [365, 1.2], [410, 1.3],[768, 3]]"
+            :perPageCustom="[[320, 1], [360, 1.2], [410, 1.3],[768, 3]]"
             id="upsell-products-carousel"
             navigation-enabled="hide"
             pagination-enabled="hide">
